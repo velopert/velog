f018471b019a482eb7642d33b89afe87de2315c4
velopert <public.velopert@gmail.com>
2018-01-27 01:10:22 +0900

    complex styling for code editor

--- velog-frontend/src/components/write/CodeEditor/CodeEditor.js
+++ velog-frontend/src/components/write/CodeEditor/CodeEditor.js
@@ -11,6 +11,7 @@ let CodeMirror = null;
 if (process.env.APP_ENV !== 'server') {
   CodeMirror = require('codemirror');
   require('codemirror/addon/scroll/simplescrollbars');
+  require('codemirror/addon/display/placeholder');
   require('codemirror/mode/markdown/markdown');
   require('codemirror/mode/javascript/javascript');
   require('codemirror/mode/jsx/jsx');
@@ -44,10 +45,11 @@ class CodeEditor extends Component<Props, State> {
 
     this.codeMirror = CodeMirror(this.editor, {
       mode: 'markdown',
-      theme: 'default',
-      lineNumbers: true, // 좌측에 라인넘버 띄우기
+      theme: 'material',
+      lineNumbers: false, // 좌측에 라인넘버 띄우기
       lineWrapping: true, // 내용이 너무 길면 다음 줄에 작성
       scrollbarStyle: 'overlay',
+      placeholder: '당신의 이야기를 적어보세요...',
     });
     this.codeMirror.on('change', this.onChange);
   }
@@ -87,7 +89,7 @@ class CodeEditor extends Component<Props, State> {
 
   render() {
     return (
-      <div className="CodeEditor">
+      <div className="CodeEditor material">
         <div className="editor" ref={(ref) => { this.editor = ref; }} />
         {/* <textarea ref={(ref) => { this.textarea = ref; }} /> */ }
       </div>
