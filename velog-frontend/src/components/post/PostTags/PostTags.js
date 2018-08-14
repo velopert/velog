8af255b173daba7c42412104304a543178efadcf
velopert <public.velopert@gmail.com>
2018-08-15 00:05:16 +0900

    added link to PostTags

--- velog-frontend/src/components/post/PostTags/PostTags.js
+++ velog-frontend/src/components/post/PostTags/PostTags.js
@@ -1,5 +1,7 @@
 // @flow
 import React from 'react';
+import { escapeForUrl } from 'lib/common';
+import { Link } from 'react-router-dom';
 import './PostTags.scss';
 
 type Props = {
@@ -7,7 +9,13 @@ type Props = {
 };
 
 const PostTags = ({ tags }: Props) => (
-  <div className="PostTags">{tags.map(t => <a key={t}>{t}</a>)}</div>
+  <div className="PostTags">
+    {tags.map(t => (
+      <Link to={`/tags/${escapeForUrl(t)}`} key={t}>
+        {t}
+      </Link>
+    ))}
+  </div>
 );
 
 export default PostTags;
