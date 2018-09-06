a6d493d0ec0a1e122ed913fcdeac8677c3ac92b2
velopert <public.velopert@gmail.com>
2018-09-06 23:23:25 +0900

    updated favicon

--- velog-ssr/src/ssr/index.ts
+++ velog-ssr/src/ssr/index.ts
@@ -17,8 +17,14 @@ function buildHtml(rendered, state, helmet) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
     <meta name="theme-color" content="#000000">
-    <link rel="manifest" href="https://velog.io/manifest.json">
+    <link rel="manifest" href="https://cdn.velog.io/manifest.json">
     <link rel="shortcut icon" href="https://cdn.velog.io/favicon.ico">
+    <link rel="apple-touch-icon" sizes="152x152" href="https://cdn.velog.io/favicons/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="https://cdn.velog.io/favicons/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="https://cdn.velog.io/favicons/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://cdn.velog.io/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="https://cdn.velog.io/favicons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://cdn.velog.io/favicons/favicon-16x16.png">
     ${helmet ? `
       ${helmet.title.toString()}
       ${helmet.meta.toString()}
