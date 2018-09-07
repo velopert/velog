97305a6faf50e8a4219e0350ddba3332bcedb326
velopert <public.velopert@gmail.com>
2018-09-08 00:18:14 +0900

    easily optimized with bracket

--- velog-backend/src/database/rawQuery/comments.js
+++ velog-backend/src/database/rawQuery/comments.js
@@ -3,17 +3,13 @@ import Sequelize from 'sequelize';
 import db from 'database/db';
 
 export const getCommentCountsOfPosts = async (postIds: string[]) => {
-  // this query must be optimized
-  const conditions = postIds
-    .map(id => `fk_post_id = '${id}'`)
-    .join('\nAND deleted = false OR ');
+  const conditions = postIds.map(id => `fk_post_id = '${id}'`).join('\nOR ');
   const query = `SELECT fk_post_id as fk_post_id, COUNT(fk_post_id) AS comments_count
 FROM comments
-WHERE ${conditions}
+WHERE deleted = false AND (${conditions})
 GROUP BY fk_post_id`;
   try {
     const rows = await db.query(query, { type: Sequelize.QueryTypes.SELECT });
-    console.log(rows);
     return rows;
   } catch (e) {
     throw e;
