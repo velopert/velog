3ca8d26e1d315acaa1e329b44fcd58b2ed37d46f
velopert <public.velopert@gmail.com>
2018-09-13 23:40:15 +0900

    close #22

--- velog-frontend/src/components/write/InputTags/InputTags.js
+++ velog-frontend/src/components/write/InputTags/InputTags.js
@@ -59,7 +59,7 @@ class InputTags extends Component<Props, State> {
     const { input } = this.state;
     const { onInsert } = this.props;
     const filtered = input.replace(
-      /[^0-9a-zA-Zㄱ-힣\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf ]/g,
+      /[^0-9a-zA-Zㄱ-힣.\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf ]/g,
       ' ',
     );
     onInsert(filtered);
