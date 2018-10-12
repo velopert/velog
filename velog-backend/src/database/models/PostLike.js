5f363f6e89a543736f76f4bca635ff473c05d7e8
Nakim <public.velopert@gmail.com>
2018-10-12 23:38:01 +0900

    Optimize like/unlike performance

--- velog-backend/src/database/models/PostLike.js
+++ velog-backend/src/database/models/PostLike.js
@@ -4,25 +4,46 @@ import db from 'database/db';
 import { User, Post } from 'database//models';
 import { primaryUUID } from 'lib/common';
 
-
-const PostLike = db.define('post_like', {
-  id: primaryUUID,
-  fk_post_id: Sequelize.UUID,
-  fk_user_id: Sequelize.UUID,
-});
+const PostLike = db.define(
+  'post_like',
+  {
+    id: primaryUUID,
+    fk_post_id: Sequelize.UUID,
+    fk_user_id: Sequelize.UUID,
+  },
+  {
+    indexes: [
+      {
+        unique: true,
+        fields: ['fk_post_id', 'fk_user_id'],
+      },
+    ],
+  },
+);
 
 PostLike.associate = function associate() {
-  PostLike.belongsTo(User, { foreignKey: 'fk_user_id', onDelete: 'CASCADE', onUpdate: 'restrict' });
-  PostLike.belongsTo(Post, { foreignKey: 'fk_post_id', onDelete: 'CASCADE', onUpdate: 'restrict' });
+  PostLike.belongsTo(User, {
+    foreignKey: 'fk_user_id',
+    onDelete: 'CASCADE',
+    onUpdate: 'restrict',
+  });
+  PostLike.belongsTo(Post, {
+    foreignKey: 'fk_post_id',
+    onDelete: 'CASCADE',
+    onUpdate: 'restrict',
+  });
 };
 
 PostLike.checkExists = function ({ userId, postId }, transaction) {
-  return PostLike.findOne({
-    where: {
-      fk_post_id: postId,
-      fk_user_id: userId,
+  return PostLike.findOne(
+    {
+      where: {
+        fk_post_id: postId,
+        fk_user_id: userId,
+      },
     },
-  }, { transaction });
+    { transaction },
+  );
 };
 
 export default PostLike;
--- velog-backend/src/router/posts/post/like/like.ctrl.js
+++ velog-backend/src/router/posts/post/like/like.ctrl.js
@@ -25,43 +25,84 @@ export const likePost = async (ctx: Context): Promise<*> => {
   const { id: userId } = ctx.user;
   const { post } = ctx;
   try {
-    // $FlowFixMe
-    await db.transaction(async (t) => {
-      const exists = await PostLike.checkExists({
-        userId,
-        postId: id,
-      });
-      if (exists) {
-        ctx.status = 409;
-        ctx.body = { name: 'ALREADY_LIKED' };
-        return;
-      }
-      await PostLike.create(
-        {
-          fk_user_id: userId,
-          fk_post_id: id,
-        },
-        {
-          transaction: t,
-        },
-      );
-      await post.like(t);
-      await PostScore.create(
-        {
-          type: TYPES.LIKE,
-          fk_user_id: userId,
-          fk_post_id: id,
-          score: 5,
-        },
-        {
-          transaction: t,
-        },
-      );
-      ctx.body = {
-        liked: true,
-        likes: post.likes,
-      };
+    const exists = await PostLike.checkExists({
+      userId,
+      postId: id,
     });
+    if (exists) {
+      ctx.status = 409;
+      ctx.body = { name: 'ALREADY_LIKED' };
+      return;
+    }
+    try {
+      await PostLike.create({
+        fk_user_id: userId,
+        fk_post_id: id,
+      });
+    } catch (e) {
+      ctx.status = 409;
+      ctx.body = { name: 'ALREADY_LIKED' };
+      return;
+    }
+
+    // count current likes
+    const count = await PostLike.count({
+      where: {
+        fk_post_id: id,
+      },
+    });
+    ctx.body = {
+      liked: true,
+      likes: count,
+    };
+    setTimeout(async () => {
+      PostScore.create({
+        type: TYPES.LIKE,
+        fk_user_id: userId,
+        fk_post_id: id,
+        score: 5,
+      });
+      post.likes = count;
+      post.save();
+    }, 0);
+
+    // $FlowFixMe
+    // await db.transaction(async (t) => {
+    //   const exists = await PostLike.checkExists({
+    //     userId,
+    //     postId: id,
+    //   });
+    //   if (exists) {
+    //     ctx.status = 409;
+    //     ctx.body = { name: 'ALREADY_LIKED' };
+    //     return;
+    //   }
+    //   await PostLike.create(
+    //     {
+    //       fk_user_id: userId,
+    //       fk_post_id: id,
+    //     },
+    //     {
+    //       transaction: t,
+    //     },
+    //   );
+    //   await post.like(t);
+    //   await PostScore.create(
+    //     {
+    //       type: TYPES.LIKE,
+    //       fk_user_id: userId,
+    //       fk_post_id: id,
+    //       score: 5,
+    //     },
+    //     {
+    //       transaction: t,
+    //     },
+    //   );
+    //   ctx.body = {
+    //     liked: true,
+    //     likes: post.likes,
+    //   };
+    // });
   } catch (e) {
     ctx.throw(500, e);
   }
@@ -73,23 +114,29 @@ export const unlikePost = async (ctx: Context): Promise<*> => {
   const { post } = ctx;
   try {
     // $FlowFixMe
-    await db.transaction(async (t) => {
-      const exists = await PostLike.checkExists({
-        userId,
-        postId: id,
-      });
-      if (!exists) {
-        ctx.status = 409;
-        ctx.body = { name: 'NOT_LIKED' };
-        return; // ⛑ VERY STUPID MISTAKE
-      }
+    const exists = await PostLike.checkExists({
+      userId,
+      postId: id,
+    });
+    if (!exists) {
+      ctx.status = 409;
+      ctx.body = { name: 'NOT_LIKED' };
+      return;
+    }
 
-      await exists.destroy({ transaction: t });
-      await post.unlike(t);
-      ctx.body = {
-        liked: false,
-        likes: post.likes,
-      };
+    await exists.destroy();
+    const count = await PostLike.count({
+      where: {
+        fk_post_id: id,
+      },
+    });
+    ctx.body = {
+      liked: false,
+      likes: count,
+    };
+    setTimeout(async () => {
+      post.likes = count;
+      post.save();
       const postScore = await PostScore.findOne({
         where: {
           type: TYPES.LIKE,
@@ -100,7 +147,7 @@ export const unlikePost = async (ctx: Context): Promise<*> => {
       if (postScore) {
         await postScore.destroy();
       }
-    });
+    }, 0);
   } catch (e) {
     ctx.throw(500, e);
   }
