4f11327795a5e336854de169865834908f93a155
Nakim <public.velopert@gmail.com>
2018-10-23 23:12:21 +0900

    Check for &nbsp; in checkEmpty

--- velog-backend/src/lib/common.js
+++ velog-backend/src/lib/common.js
@@ -96,7 +96,8 @@ export function checkEmpty(text: string) {
     .replace(
       /([\u3164\u115F\u1160\uFFA0\u200B\u0001-\u0008\u000B-\u000C\u000E-\u001F]+)/g,
       '',
-    );
+    )
+    .replace(/&nbsp;/, '');
   if (replaced === '') return true;
   return false;
 }
